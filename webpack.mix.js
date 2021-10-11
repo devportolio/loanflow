e3ce2f822766ec7274d1d9907f90bf975ba154d3
Loan Flow <loanflow.86@gmail.com>
2021-10-12 00:15:40 +0200

    fixed hot reload

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,3 +14,8 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .react()
     .sass('resources/sass/app.scss', 'public/css')
+
+
+mix.browserSync({
+    proxy: 'http://loanflow.local'
+});
